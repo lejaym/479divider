7c9db4decf070c1b4f2c4b5cb7161ad8aa478eec
Marc Lejay <lejay.marc@gmail.com>
2015-04-10 17:58:08 -0700

    Changes made and labels added for debugging

---
+++ cells/smlogic.cmd
@@ -0,0 +1,36 @@
+stepsize 50
+w load shift inbit add sel1 sel0 start sign SB1 SB0 nextSB1 nextSB0
+vector in start sign SB1 SB0
+set in 0000
+s
+set in 0001
+s
+set in 0010
+s
+set in 0011
+s
+set in 0100
+s
+set in 0101
+s
+set in 0110
+s
+set in 0111
+s
+set in 1000
+s
+set in 1001
+s
+set in 1010
+s
+set in 1011
+s
+set in 1100
+s
+set in 1101
+s
+set in 1110
+s
+set in 1111
+s
+analyzer load shift inbit add sel1 sel0 start sign SB1 SB0 nextSB1 nextSB0
--- layout/divider.cmd
+++ layout/divider.cmd
@@ -1,22 +1,38 @@
 stepsize 1000
-
-| Set up vectors
-
 vector divisorin divisorin_{6:0}
 vector dividendin dividendin_{7:0}
 vector remainder remainder_{6:0}
 vector quotient quotient_{7:0}
 
-| An initial clock pulse to get things going
-| as in the specification (verilog) test bench
+vector divregout divregout{7:0}
+vector sel sel{1:0}
+vector rmuxout rmuxout{7:0}
+vector qmuxout qmuxout{7:0}
+vector state SB{1:0}
+vector sumout sign sum{6:0}
 
+|smlogic related: clk reset start state add shift sel inbit load
+w clk reset start state sign quotient remainder qmuxout rmuxout divregout sumout add
+
+|w  rmuxout qmuxout divregout quotient remainder SB0 SB1 sign start reset clk 
+|sum0 sum1 sum2 sum3 sum4 sum5 sum6 divregout0 divregout1 divregout2 divregout3 divregout4 divregout5 divregout6 divregout7
+|clkload nextsb1 nextsb0
+settle 10
+
+l start
+h clk 
+h reset
 s
-h clk
+l reset
 s
 l clk
 s
 
-| Start the process by asserting Start and waiting one clock cycle
+| Set up vectors
+print "load dividend/divisor"
+set divisorin 0000011
+set dividendin 00010000
+
 
 h start
 s
@@ -24,15 +40,13 @@ h clk
 s
 l clk
 s
-
-| Now, lower start, and set the divisor and dividend.  For your
-| own tests, you should change these numbers here.  In this example,
-| we are dividing 16 by 3.
-
+h clk
+s
+l clk
+s
 l start
-set divisorin 0000011
-set dividendin 00010000
 
+print "17 cycles"
 | Now, wait 17 cycles
 
 s
@@ -40,84 +54,82 @@ h clk
 s
 l clk
 s
+h clk
+s
+l clk
 s
 h clk
 s
 l clk
 s
+h clk
+s
+l clk
 s
 h clk
 s
 l clk
 s
+h clk
+s
+l clk
 s
 h clk
 s
 l clk
 s
-
+h clk
+s
+l clk
 s
 h clk
 s
 l clk
 s
+h clk
+s
+l clk
 s
 h clk
 s
 l clk
 s
+h clk
+s
+l clk
 s
 h clk
 s
 l clk
 s
+h clk
+s
+l clk
 s
 h clk
 s
 l clk
 s
-
+h clk
+s
+l clk
 s
 h clk
 s
 l clk
 s
+print "extra cycles"
+h clk
+s
+l clk
 s
 h clk
 s
 l clk
 s
-s
 h clk
 s
 l clk
-s
-s
-h clk
-s
-l clk
-s
-
-s
-h clk
-s
-l clk
-s
-s
-h clk
-s
-l clk
-s
-s
-h clk
-s
-l clk
-s
-s
-h clk
-s
-l clk
-s
 s
 h clk
 s
