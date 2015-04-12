04619b6ca425f5a0231d1af5ae41a02c430523bf
Marc Lejay <lejay.marc@gmail.com>
2015-04-11 22:45:35 -0700

    Inverted statemachine clock

--- layout/divider.cmd
+++ layout/divider.cmd
@@ -11,7 +11,7 @@ vector remainder remainder_{6:0}
 vector quotient quotient_{7:0}
 
 vector divregout divregout{7:0}
-vector validregs valid validregs{8:0}
+vector validregs valid validregs{7:0}
 vector sel sel{1:0}
 vector rmuxout rmuxout{7:0}
 vector qmuxout qmuxout{7:0}
@@ -21,39 +21,37 @@ vector qregin qregin{7:0}
 vector rregin rregin{7:0}
 
 |smlogic related: clk reset start state add shift sel inbit load
-w clk reset start state sign add quotient remainder qregin qmuxout rregin rmuxout divregout sumout valid shift inbit
-
 |w  rmuxout qmuxout divregout quotient remainder SB0 SB1 sign start reset clk 
-|sum0 sum1 sum2 sum3 sum4 sum5 sum6 divregout0 divregout1 divregout2 divregout3 divregout4 divregout5 divregout6 divregout7
 |clkload nextsb1 nextsb0
+
+
+w clk reset start state sel sign add quotient remainder qregin qmuxout rregin rmuxout divregout sumout valid shift topS0
+
 
 | FOR ASYNC RESET, CLK MUST BE SET HI or LOW. ASYNC RESET DOES NOT WORK
 | IF CLK IS X
 
-set dividendin 00000000
-set divisorin 0000000
-
-l start
-h clk 
-h reset
-s
-l clk
-l reset
-s
 
 | Set up vectors
 print "load dividend/divisor"
-set divisorin 0000011
-set dividendin 00010000
-
+set divisorin 0000010
+|0000011
+set dividendin 00001000 
+|00010000
 
 h start
 s
 h clk
 s
-l start
+h reset
+l clk
+s
+l reset
+h clk
 s
 l clk
+l start
+
 s
 
 print "17 cycles"
@@ -72,59 +70,11 @@ h clk
 s
 l clk
 s
-h clk
-s
-l clk
-s
-h clk
-s
-l clk
-s
-h clk
-s
-l clk
-s
-h clk
-s
-l clk
-s
-h clk
-s
-l clk
-s
-h clk
-s
-l clk
-s
-h clk
-s
-l clk
-s
-h clk
-s
-l clk
-s
-h clk
-s
-l clk
-s
-h clk
-s
-l clk
-s
-Print "LAST CYCLE"
-h clk
-s
-l clk
-s
-
-| repeat h clk s l clk s 14 times
 
 
 | Print out results
 
 w remainder quotient
 s
-tcap
 exit
 
